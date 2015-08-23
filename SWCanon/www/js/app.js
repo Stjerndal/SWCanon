7ed28f57ca9c3cb9b602aeee66ced159ba6a3c20
Andreas Stjerndal <stjernd@kth.se>
2015-08-23 23:58:35 +0200

    Adding tab routing.

--- SWCanon/www/js/app.js
+++ SWCanon/www/js/app.js
@@ -17,3 +17,53 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+  .state('tab.list', {
+      url: '/list',
+      views: {
+        'list-tab' : {
+          templateUrl: 'templates/list.html',
+          controller: 'ListController'
+        }
+      }
+    })
+    .state('tab.detail', {
+      url: '/list/:aId',
+      views: {
+        'list-tab' : {
+          templateUrl: 'templates/detail.html',
+          controller: 'ListController'
+        }
+      }
+    })
+
+  .state('tab.settings', {
+      url: '/settings',
+      views: {
+        'settings-tab' : {
+          templateUrl: 'templates/settings.html',
+          controller: 'SettingsController'
+        }
+      }
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/list');
+
+});
